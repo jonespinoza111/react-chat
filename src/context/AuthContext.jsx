2246ee16b18b766d8e17e46c9aafb2a840a27f2e
jonespinoza111 <jonespinoza111@gmail.com>
2023-01-12 11:07:36 -0500

    created auth for users

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,40 @@
+import React, { createContext, useCallback } from "react";
+import { useState } from "react";
+
+export const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+    const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
+    const [userInfo, setUserInfo] = useState(null);
+
+    const checkAuthUser = useCallback(() => {
+        const currentUser = localStorage.getItem("currentUser");
+        if (currentUser) {
+            console.log("checking right now and there is user");
+            setIsUserLoggedIn(true);
+            console.log("this is what is getting parsed right now ", JSON.parse(atob(currentUser.split(".")[1])));
+            setUserInfo(JSON.parse(atob(currentUser.split(".")[1])));
+        }
+    }, []);
+
+    const logout = (history, socket) => {
+        localStorage.removeItem("currentUser");
+        socket.emit('offline', userInfo.uid);
+        setIsUserLoggedIn(false);
+        history.push({ pathname: "/" });
+    };
+    return (
+        <AuthContext.Provider
+            value={{
+                isUserLoggedIn,
+                userInfo,
+                checkAuthUser,
+                logout,
+            }}
+        >
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
