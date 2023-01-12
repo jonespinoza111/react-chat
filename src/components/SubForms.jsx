a738c2bd9de801e08db90c254c5a9fca574f8b3e
jonespinoza111 <jonespinoza111@gmail.com>
2023-01-12 00:03:46 -0500

    Assets and style changes

---
+++ src/components/SubForms.jsx
@@ -0,0 +1,115 @@
+import React from 'react'
+import { avatarOptions } from '../data/avatarOptions';
+
+export const SubForm1 = ({
+    currentSubForm,
+    setFirstName,
+    setLastName,
+    setEmail,
+    firstName,
+    lastName,
+    email
+}) => {
+    console.log("this is the currentSubForm", currentSubForm);
+    if (currentSubForm !== 1) {
+        return null;
+    }
+    return (
+        <>
+            <div className="form-input-container">
+                <label htmlFor="firstname">First Name</label>
+                <input
+                    id="firstname"
+                    className="form-input input"
+                    placeholder='enter first name'
+                    // value={"firstname"}
+                    type="text"
+                    required
+                    // onChange={(e) => setFirstName(e.target.value)}
+                />
+            </div>
+            <div className="form-input-container">
+                <label htmlFor="lastname">Last Name</label>
+                <input
+                    id="lastname"
+                    className="form-input input"
+                    placeholder='enter last name'
+                    // value={"lastname"}
+                    type="text"
+                    required
+                    // onChange={(e) => setLastName(e.target.value)}
+                />
+            </div>
+            <div className="form-input-container">
+                <label htmlFor="email">Email</label>
+                <input
+                    id="email"
+                    className="form-input input"
+                    placeholder='enter email'
+                    // value={"email"}
+                    type="email"
+                    required
+                    // onChange={(e) => setEmail(e.target.value)}
+                />
+            </div>
+        </>
+    );
+};
+
+
+export const SubForm2 = ({ currentSubForm, setUsername, setPassword, username, password }) => {
+    if (currentSubForm !== 2) {
+        return null;
+    }
+    return (
+        <>
+            <div className="form-input-container">
+                <label htmlFor="username">Username</label>
+                <input
+                    id="username"
+                    className="form-input"
+                    // value={username}
+                    placeholder='create username'
+                    type="text"
+                    required
+                    // onChange={(e) => setUsername(e.target.value)}
+                />
+            </div>
+            <div className="form-input-container">
+                <label htmlFor="password">Password</label>
+                <input
+                    id="password"
+                    className="form-input"
+                    // value={password}
+                    placeholder='create password'
+                    type="password"
+                    required
+                    // onChange={(e) => setPassword(e.target.value)}
+                />
+            </div>
+        </>
+    );
+};
+
+
+export const SubForm3 = ({ currentSubForm, avatarPicked, setAvatarPicked }) => {
+    if (currentSubForm !== 3) {
+        return null;
+    }
+    return (
+        <div className="options-container">
+            {avatarOptions.map(({ optionNum, src, alt }) => {
+                return (
+                    <img
+                        key={optionNum}
+                        className={`avatar-option current-choice`}
+                        // className={`avatar-option ${(avatarPicked === optionNum) && 'current-choice'}`}
+                        src={require(`../assets/${src}`)}
+                        alt={alt}
+                        // onClick={() => setAvatarPicked(optionNum)}
+                    />
+                );
+            })}
+        </div>
+    );
+};
--- src/pages/LoginPage/Login.jsx
+++ src/pages/LoginPage/Login.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Login.scss';
 
 const Login = () => {
@@ -6,23 +7,22 @@ const Login = () => {
     <div className='page login-page'>
       <div className='container'>
         <div className='left-side'>
-          <div className='login-form-container'>
-            <h3>Welcome Back</h3>
+          <div className='login-form-container form-container'>
+            <h3>Welcome back</h3>
             <h4>Welcome back! Please enter your details.</h4>
             <form className="login-form">
-                <h1 className="form-title">Login</h1>
                 <div className="form-input-container">
-                    <label htmlFor="username">*Username</label>
+                    <label htmlFor="username">Username</label>
                     <input
                         id="username"
                         className="form-input input"
-                        placeholder=""
+                        placeholder="Enter your email"
                         type="text"
                         required                     
                     />
                 </div>
                 <div className="form-input-container">
-                    <label htmlFor="firstname">*Password</label>
+                    <label htmlFor="firstname">Password</label>
                     <input
                         id="password"
                         className="form-input input"
@@ -33,13 +33,23 @@ const Login = () => {
                 </div>
                 <div className="form-button-container">
                     <button className="form-button login-form-button" type="submit">
-                        Login
+                      Sign In
                     </button>
+                </div>
+                <div className='request-sign-up'>
+                  <span>Don't have an account?</span>
+                  <Link className="form-link" to="/signup">
+                    Sign up instead
+                  </Link>
                 </div>
             </form>
           </div>
         </div>
-        <div className='right-side'>Side 2</div>
+        <div className='right-side'>
+          <div className='image-container'>
+            <img src={require('../../assets/Begin-chat.png')} width={400} height={400} alt="2 people chatting online" />
+          </div>
+        </div>
       </div>
     </div>
   )
