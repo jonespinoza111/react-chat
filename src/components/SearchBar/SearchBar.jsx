da6e24eaa2dbe8369f9b51b24aac4b8e0f1eb2fc
jonespinoza111 <jonespinoza111@gmail.com>
2023-03-09 17:52:41 -0500

    searchbar icon added

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,59 @@
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./SearchBar.scss";
+
+const SearchBar = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [searchError, setSearchError] = useState(null);
+  const navigate = useNavigate();
+
+  const searchUser = async (e) => {
+    e.preventDefault();
+    console.log("humpty dumpty", inputValue);
+    try {
+      const response = await fetch(
+        `http://localhost:5000/users/username/${inputValue.toLowerCase()}`
+      );
+
+      const body = await response.json();
+
+      if (body.success) {
+        setSearchError(null);
+        navigate(`/user/${body.user._id}`);
+        console.log("User you searched", body.user);
+      }
+
+      if (!body.success) {
+        let error = "User not found";
+        throw error;
+      }
+    } catch (err) {
+      setSearchError(err);
+    }
+  };
+
+  return (
+    <>
+      <form className="search" onSubmit={searchUser}>
+        <button type="submit" disabled={inputValue.length < 1} className="search-icon-container">
+          <FontAwesomeIcon
+            className="search-icon"
+            icon={faMagnifyingGlass}
+          />
+        </button>
+        <input
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          type="text"
+          className="search-bar"
+          placeholder="Enter a username"
+        />
+      </form>
+      <h3 className="search-error">{searchError}</h3>
+    </>
+  );
+};
+
+export default SearchBar;
